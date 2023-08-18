394eb8a1bf01fd7148838950f393509ac11a7802
prateeky477 <prateeky477@gmail.com>
2023-08-18 22:42:46 +0530

    up

--- server/app.js
+++ server/app.js
@@ -31,13 +31,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-
 app.use(
   session({
     secret: process.env.SECRET,
