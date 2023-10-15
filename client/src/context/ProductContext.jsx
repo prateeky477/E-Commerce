d921d5b489dfa0931de5844169c7f3665f46bb2d
your name goes here <prateeky477@gmail.com>
2023-10-16 00:33:44 +0530

    url prod

--- client/src/context/ProductContext.jsx
+++ client/src/context/ProductContext.jsx
@@ -12,7 +12,7 @@ const ProductProvider = ({ children }) => {
 
         const fetchProducts = async () => {
             try {
-                const response = await axios.get("api/products")
+                const response = await axios.get("https://e-commerce-aaoa.vercel.app/api/products")
                 console.log(response.data.products);
                 setProducts(response.data.products)
                 console.log(response.data)
